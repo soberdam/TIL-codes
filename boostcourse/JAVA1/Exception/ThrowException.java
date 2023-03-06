df6379daf445d830172ca2b543c5143f9de36438
조담현 <j38903639@gmail.com>
2023-03-07 01:42:02 +0900

    Delete boostcourse/JAVA1 directory

--- boostcourse/JAVA1/Exception/ThrowException.java
+++
@@ -1,9 +0,0 @@
-
-public class ThrowException {
-
-	public static void main(String[] args) {
-		throw new RuntimeException("오류가 있습니다.");
-
-	}
-
-}
--- boostcourse/JAVA1/ProjectOOP/OthersOOP.java
+++
@@ -1,9 +0,0 @@
-
-public class OthersOOP {
-
-	public static void main(String[] args) {
-		
-		
-	}
-
-}
